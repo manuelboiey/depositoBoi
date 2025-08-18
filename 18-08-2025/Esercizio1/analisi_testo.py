7da029eb6bb8e3eef4dac8fc0e9fdb236fb1c5d4
manuelboiey <Manuel.Boi@it.ey.com>
2025-08-18 12:26:26 +0200

    feat(count): Count lines of file content

--- 18-08-2025/Esercizio1/analisi_testo.py
+++ 18-08-2025/Esercizio1/analisi_testo.py
@@ -2,6 +2,7 @@
 import os
 
 
+# Functions
 def read_file(path="input.txt"):
     """Read file content, if file exists."""
     if not os.path.exists(path):
@@ -10,9 +11,16 @@ def read_file(path="input.txt"):
 
     with open(path, "r", encoding="utf-8") as file:
         content = file.read()
-
         return content
 
+def count_lines(text: str) -> int:
+    """Count line number."""
+    return len(text.splitlines())
 
+
+# Main
 if __name__ == "__main__":
     content = read_file("input.txt")
+    if content is not None:
+        line_count = count_lines(content)
+        print(f"Number of lines: {line_count}")
