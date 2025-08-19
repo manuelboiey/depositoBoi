d7666a318fc2c6ae500a6b41aff2a857b68307ce
manuelboiey <Manuel.Boi@it.ey.com>
2025-08-19 11:08:07 +0200

    Improve code cleaness of exercise 1

--- 19-08-2025/Esercizio1/energy_consumption.py
+++ 19-08-2025/Esercizio1/energy_consumption.py
@@ -2,19 +2,13 @@ import pandas as pd
 import numpy as np
 
 # Read dataset
-df = pd.read_csv("AEP_hourly.csv")
+df = pd.read_csv("./AEP_hourly.csv")
 
 # Convert to datetime
 df["Datetime"] = pd.to_datetime(df["Datetime"])
 
-# Add column only of the data
-df["Date"] = df["Datetime"].dt.date
-
 # Compute daily average
-daily_mean = df.groupby("Date")["AEP_MW"].mean().rename("Daily_Mean")
-
-# Merge daily average with original DataFrame
-df = df.merge(daily_mean, on="Date")
+df["Daily_Mean"] = df.groupby(df["Datetime"].dt.date)["AEP_MW"].transform("mean")
 
 # Difference with respect to average
 df["Delta"] = df["AEP_MW"] - df["Daily_Mean"]
@@ -25,8 +19,6 @@ df["Status"] = np.select(
     ["Above Average", "Below Average"],
     default="Equal to Average"
 )
-
-df.drop(columns=["Date", "Daily_Mean"], inplace=True)
 
 # Save results to a new CSV file
 df.to_csv("AEP_hourly_tagged.csv", index=False)
